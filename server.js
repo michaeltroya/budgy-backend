05ac305076dde4f434bef14c0b623dcf8db7cf85
Michael <michaeltroya13@gmail.com>
2020-01-06 14:36:38 -0500

    use .env file

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
+const passport = require('passport');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 //DB
-const db = require('./config/keys').MongoURI;
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
