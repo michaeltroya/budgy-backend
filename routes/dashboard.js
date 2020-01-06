9d2e57fc9763fa7106541e47782f1b5f434e7529
Michael <michaeltroya13@gmail.com>
2020-01-06 18:06:12 -0500

    change file names

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 //GET DASHBOARD INFO
 router.get('/', (req, res) => {
   res.send('home');
+});
+
+router.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { user } = req;
+  res.status(200).send({ user });
 });
 
 module.exports = router;
