63ed74432cc1fed54193fc8ef4b5837c3aafc875
Michael <michaeltroya13@gmail.com>
2020-01-06 14:33:14 -0500

    add passport config

---
+++ config/passport.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const jwtStrategy = require('passport-jwt').Strategy;
+const extractJwt = require('passport-jwt').ExtractJwt;
+
+const User = require('../models/User');
+
+const opts = {};
+opts.jwtFromRequest = extractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.SECRET_KEY;
+
+module.exports = passport => {
+  passport.use(
+    new jwtStrategy(opts, (jwtPayload, done) => {
+      User.findById(jwtPayload.id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    })
+  );
+};
